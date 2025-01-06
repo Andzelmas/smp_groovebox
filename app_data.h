c1f98c4bd92260b94b97333c289568cf533c8f0c
Andzelmas <brukstus@gmail.com>
2025-01-06 15:07:24 +0200

    didnt add app_data.h with the last commit

--- app_data.h
+++ app_data.h
@@ -44,34 +44,23 @@ int app_plug_init_plugin(APP_INFO* app_data, const char* plugin_uri, unsigned ch
 int app_plug_load_preset(APP_INFO* app_data, const char* preset_uri, unsigned int cx_type, const int plug_id);
 //initialize a sample on sampler context
 int app_smp_sample_init(APP_INFO* app_data, const char* samp_path, int in_id);
-//get the apropriate parameter container depending on the context (trk, sampler etc.)
-static PRM_CONTAIN* app_return_param_container(APP_INFO* app_data, unsigned char cx_type, int cx_id);
-//writes to a ring buffer a parameter change, with new param_value,
-//param_op is what to do with the value, check paramOperType in the types.h
-//if rt_to_ui is 1 the ring buffer will be rt_to_ui_ring otherwise it will be ui_to_rt_ring
-//only rt thread should write to the rt_to_ui_ring, and only the ui thread to the ui_to_rt_ring
-int app_param_set_value(APP_INFO* app_data, unsigned char cx_type, int cx_id, int param_id, float param_value,
-			unsigned char param_op, unsigned int rt_to_ui);
+//call appropriate cx_type context function to set parameter value
+int app_param_set_value(APP_INFO* app_data, unsigned char cx_type, int cx_id, int param_id, float param_value, unsigned char param_op);
 //return the parameter increment amount (by how much the value increases or decreases)
-SAMPLE_T app_param_get_increment(APP_INFO* app_data, unsigned char cx_type, int cx_id, int param_id, unsigned int rt_param);
+SAMPLE_T app_param_get_increment(APP_INFO* app_data, unsigned char cx_type, int cx_id, int param_id);
 //get value, the val_type returns what type of value for display purposes.
-//rt_param - should the value be from the rt_param array, only the rt thread should get values from there.
 //curved == 1 the parameter should be returned from the curve_table if there is one on the parameter
-SAMPLE_T app_param_get_value(APP_INFO* app_data, unsigned char cx_type, int cx_id, int param_id,
-			     unsigned char* val_type, unsigned int curved, unsigned int rt_param);
+SAMPLE_T app_param_get_value(APP_INFO* app_data, unsigned char cx_type, int cx_id, int param_id, unsigned char* val_type, unsigned int curved);
 //get the parameter id given the context type, cx id and parameter id. Returns -1 if no such parameter is found
-int app_param_id_from_name(APP_INFO* app_data, unsigned char cx_type, int cx_id, const char* param_name, unsigned int rt_param);
+int app_param_id_from_name(APP_INFO* app_data, unsigned char cx_type, int cx_id, const char* param_name);
 //get the string of the String_Return_Type parameter
-const char* app_param_get_string(APP_INFO* app_data, unsigned char cx_type, int cx_id, int param_id,
-				 unsigned int rt_param);
-//returns all names of the cx of the context type, for example all names of the oscillators of the synth
-const char** app_context_return_names(APP_INFO* app_data, unsigned char cx_type, int* cx_num);
-//returns all the parameters values, types and names to the param_names, param_vals and param_types arrays
-//user has to free these arrays
-//if the current name is NULL something went wrong. Also if the parameter val is -1 and the param type is 0 something
-//went wrong. This will get the parameters on the ui_params array
+const char* app_param_get_string(APP_INFO* app_data, unsigned char cx_type, int cx_id, int param_id);
+//returns all the parameters values, types and names to the param_names, param_vals and param_types arrays. User has to free these arrays
+//if the current name is NULL something went wrong. Also if the parameter val is -1 and the param type is 0 something went wrong.
 int app_param_return_all_as_string(APP_INFO* app_data, unsigned char cx_type, int cx_id, char*** param_names, char*** param_vals,
 				   char*** param_types, unsigned int* param_num);
+//returns all names of the cx of the context type, for example all names of the oscillators of the synth
+const char** app_context_return_names(APP_INFO* app_data, unsigned char cx_type, int* cx_num);
 //return the name of a context by its cx_id (for example the name of a sample by its id, or name of a plugin by its id)
 //if add_id == 1 will add the id to the end of the string
 char* app_return_cx_name(APP_INFO* app_data, unsigned char cx_type, int cx_id, unsigned int add_id);
@@ -90,13 +79,6 @@ char* app_return_short_port(APP_INFO* app_data, const char* full_port_name);
 const char** app_return_context_ports(APP_INFO* app_data, unsigned int* name_num, unsigned int cx_type, unsigned int cx_id);
 //realtime callback for the track context, as usual has to use only _rt functions
 int trk_audio_process_rt(jack_nframes_t nframes, void *arg);
-//go through the transport control and setting parameters and play, stop, pause etc.
-//also process the metronome
-static int app_transport_control_rt(APP_INFO* app_data, NFRAMES_T nframes);
-//function that reads ring buffer and calls the appropriate context functions to set their rt_param or ui_param arrays
-//to the new values. If rt_to_ui == 1 read the rt_to_ui ring buffer, otherwise read the ui_to_rt ring buffer.
-//The rt_to_ui should be read only by the ui thread, and ui_to_rt only by the rt thread.
-static int app_read_ring_buffer(APP_INFO* app_data, unsigned int rt_to_ui);
 //Reads the rt_to_ui buffer and saves any context param values to their ui_params arrays.
 int app_update_ui_params(APP_INFO* app_data);
 //realtime callback for the plugin host data, as always only _rt functions inside
