5ecbbd9ab0edddad3bacdda8bf87d57ecfc80142
Andzelmas <brukstus@gmail.com>
2025-10-15 22:11:06 +0300

    formatting

--- util_funcs/log_funcs.c
+++ util_funcs/log_funcs.c
@@ -3,80 +3,88 @@
 #include <stdlib.h>
 #define LOGFILE "log_file.log"
 
-int log_append_logfile(const char* add_string, ...){
+int log_append_logfile(const char *add_string, ...) {
     va_list args;
     va_start(args, add_string);
-    FILE* fp = NULL;
+    FILE *fp = NULL;
     fp = fopen(LOGFILE, "a");
-    if(!fp)fp = fopen(LOGFILE, "w");
-    if(!fp)return -1;
+    if (!fp)
+        fp = fopen(LOGFILE, "w");
+    if (!fp)
+        return -1;
     int rc = vfprintf(fp, add_string, args);
     va_end(args);
     fclose(fp);
     return rc;
 }
 
-char* log_getline_logfile(int line_num){
-    FILE* fp = NULL;
+char *log_getline_logfile(int line_num) {
+    FILE *fp = NULL;
     fp = fopen(LOGFILE, "r");
-    if(!fp)return NULL;
-    char* return_line  = NULL;
+    if (!fp)
+        return NULL;
+    char *return_line = NULL;
     unsigned int curr_line = 0;
     unsigned int line_size = 0;
     unsigned int found = 0;
     char c;
-    while(!feof(fp)){
-	c = fgetc(fp);
-	if(curr_line == line_num){
-	    line_size += 1;
-	    if(!return_line){
-		return_line = (char*)malloc(sizeof(char));
-		if(!return_line)return NULL;
-	    }
-	    else{
-		return_line = (char*)realloc(return_line, sizeof(char)*line_size);
-		if(!return_line)return NULL;
-	    }
-	    if(c == '\n'){
-		return_line[line_size-1] = '\0';
-		found = 1;
-		break;
-	    }
-	    if(found==1)break;
-	    return_line[line_size-1] = c;
-	}
-	else{
-	    if(c == '\n'){
-		curr_line += 1;
-	    }
-	}
-	if(found==1)break;
+    while (!feof(fp)) {
+        c = fgetc(fp);
+        if (curr_line == line_num) {
+            line_size += 1;
+            if (!return_line) {
+                return_line = (char *)malloc(sizeof(char));
+                if (!return_line)
+                    return NULL;
+            } else {
+                return_line =
+                    (char *)realloc(return_line, sizeof(char) * line_size);
+                if (!return_line)
+                    return NULL;
+            }
+            if (c == '\n') {
+                return_line[line_size - 1] = '\0';
+                found = 1;
+                break;
+            }
+            if (found == 1)
+                break;
+            return_line[line_size - 1] = c;
+        } else {
+            if (c == '\n') {
+                curr_line += 1;
+            }
+        }
+        if (found == 1)
+            break;
     }
     fclose(fp);
-    return return_line;    
+    return return_line;
 }
 
-unsigned int log_calclines_logfile(){
-    FILE* fp = NULL;
+unsigned int log_calclines_logfile() {
+    FILE *fp = NULL;
     fp = fopen(LOGFILE, "r");
-    if(!fp)return 0;
-    char* return_line  = NULL;
+    if (!fp)
+        return 0;
+    char *return_line = NULL;
     unsigned int curr_line = 0;
     char c;
-    while(!feof(fp)){
-	c = fgetc(fp);
-	if(c == '\n'){
-	    curr_line += 1;
-	}
+    while (!feof(fp)) {
+        c = fgetc(fp);
+        if (c == '\n') {
+            curr_line += 1;
+        }
     }
     fclose(fp);
-    return curr_line;    
+    return curr_line;
 }
 
-int log_clear_logfile(){
-    FILE* fp= NULL;
+int log_clear_logfile() {
+    FILE *fp = NULL;
     fp = fopen(LOGFILE, "w");
-    if(!fp)return -1;
+    if (!fp)
+        return -1;
     fclose(fp);
     return 0;
 }
--- util_funcs/log_funcs.h
+++ util_funcs/log_funcs.h
@@ -1,8 +1,11 @@
 //function to append to the log file, should end with the \n line terminator
 int log_append_logfile(const char* add_string, ...);
+
 //function to get a line from the log file
 char* log_getline_logfile(int line_num);
+
 //calculate how many lines in the logfile
 unsigned int log_calclines_logfile();
+
 //function to clear the logfile so its empty
 int log_clear_logfile();
